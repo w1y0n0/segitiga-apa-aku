bec4069a36643edf62fad19bc627ef73d48040fc
w1y0n0 <wiyono30@gmail.com>
2024-12-09 12:07:33 +0700

    penerapan  looping untuk semua sisi segitiga

--- tests/detectTriangle.test.js
+++ tests/detectTriangle.test.js
@@ -1,7 +1,9 @@
 const detectTriangle = (sideA, sideB, sideC) => {
-    if (sideA < 1 || sideB < 1 || sideC < 1) {
-        throw new Error('Strange Triangle');
-    }
+    [sideA, sideB, sideC].forEach((side) => {
+        if (side < 1) {
+            throw new Error('Strange Triangle');
+        }
+    });
 };
 
 describe('Detect the triangle', () => {
