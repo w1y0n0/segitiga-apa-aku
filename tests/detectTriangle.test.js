f985e26b58090da00b402f9ef422c6bd5deba8de
w1y0n0 <wiyono30@gmail.com>
2024-12-09 12:32:37 +0700

    mengubah urutan pemeriksaan dalam loop

--- tests/detectTriangle.test.js
+++ tests/detectTriangle.test.js
@@ -1,15 +1,13 @@
 const detectTriangle = (sideA, sideB, sideC) => {
     [sideA, sideB, sideC].forEach((side) => {
+        if (!Number.isInteger(side)) {
+            throw new Error('Sides have to be Integer');
+        }
+
         if (side < 1) {
             throw new Error('Strange Triangle');
         }
     });
-
-    // B. Kita terapkan secara bertahap dan dengan kode seperlunya saja
-    // 2. sideA, 4. sideB, 6. sideC
-    if (!Number.isInteger(sideA) || !Number.isInteger(sideB) || !Number.isInteger(sideC)) {
-        throw new Error('Sides have to be Integer');
-    }
 };
 
 describe('Detect the triangle', () => {
@@ -19,13 +17,11 @@ describe('Detect the triangle', () => {
         expect(() => detectTriangle(1, 2, -2)).toThrowError('Strange Triangle');
     });
 
-    // A. kita buat tesnya terlebih dahulu
     it('Should fail if the sides are not integer', () => {
-        // 1. Test for sideA
         expect(() => detectTriangle('a', 2, 2)).toThrowError('Sides have to be Integer');
-        // 3. Test for sideB
         expect(() => detectTriangle(1, 'b', 2)).toThrowError('Sides have to be Integer');
-        // 5. Test for sideC
         expect(() => detectTriangle(1, 2, 'c')).toThrowError('Sides have to be Integer');
+
+        expect(() => detectTriangle(' ', 2, 2)).toThrowError('Sides have to be Integer');
     });
 });
