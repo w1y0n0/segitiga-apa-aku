e93ca610a2a252f43eedbdea0190764487504349
w1y0n0 <wiyono30@gmail.com>
2024-12-09 12:20:32 +0700

    1.b. Bila salah satu sisi tidak bernilai integer, hasilnya gagal

--- tests/detectTriangle.test.js
+++ tests/detectTriangle.test.js
@@ -4,6 +4,12 @@ const detectTriangle = (sideA, sideB, sideC) => {
             throw new Error('Strange Triangle');
         }
     });
+
+    // B. Kita terapkan secara bertahap dan dengan kode seperlunya saja
+    // 2. sideA, 4. sideB, 6. sideC
+    if (!Number.isInteger(sideA) || !Number.isInteger(sideB) || !Number.isInteger(sideC)) {
+        throw new Error('Sides have to be Integer');
+    }
 };
 
 describe('Detect the triangle', () => {
@@ -11,5 +17,15 @@ describe('Detect the triangle', () => {
         expect(() => detectTriangle(-1, 2, 2)).toThrowError('Strange Triangle');
         expect(() => detectTriangle(1, -2, 2)).toThrowError('Strange Triangle');
         expect(() => detectTriangle(1, 2, -2)).toThrowError('Strange Triangle');
+    });
+
+    // A. kita buat tesnya terlebih dahulu
+    it('Should fail if the sides are not integer', () => {
+        // 1. Test for sideA
+        expect(() => detectTriangle('a', 2, 2)).toThrowError('Sides have to be Integer');
+        // 3. Test for sideB
+        expect(() => detectTriangle(1, 'b', 2)).toThrowError('Sides have to be Integer');
+        // 5. Test for sideC
+        expect(() => detectTriangle(1, 2, 'c')).toThrowError('Sides have to be Integer');
     });
 });
